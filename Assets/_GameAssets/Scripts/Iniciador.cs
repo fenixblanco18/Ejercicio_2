ab51886654642f950c0be4a04dfe1d40492745ba
fenixblanco18 <montero.helena9@gmail.com>
2024-03-06 20:24:06 +0100

    Imagen inicial y botones

---
+++ Assets/_GameAssets/Scripts/Iniciador.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Iniciador : MonoBehaviour
+{
+    public void IniciarJuego(){
+        
+        SceneManager.LoadScene(1);
+
+    }
+}
